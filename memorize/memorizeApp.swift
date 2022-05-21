bacdeb329ac97da6258b4b193d8fc7ec8ddbf004
leobmaffei <leobmaffei@gmail.com>
2022-05-20 21:04:47 -0300

    observe changes in the model

--- memorize/memorizeApp.swift
+++ memorize/memorizeApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct memorizeApp: App {
+    
+    let game = EmojiMemoryGame()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: game)
         }
     }
 }
