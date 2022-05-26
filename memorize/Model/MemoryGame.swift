f2368af406989ceb492e0babe8fe98da4fb51bdf
leobmaffei <leobmaffei@gmail.com>
2022-05-26 16:08:17 -0300

    animation

--- memorize/Model/MemoryGame.swift
+++ memorize/Model/MemoryGame.swift
@@ -51,7 +51,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     struct Card: Identifiable {
 
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         var content: CardContent
         var id: Int
